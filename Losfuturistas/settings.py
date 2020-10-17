79a6439b96930cd6b2a61fc5f2934760a36b797d
LuisRG30 <58560972+LuisRG30@users.noreply.github.com>
2020-10-16 22:35:17 -0500

    Added * as host

--- Losfuturistas/settings.py
+++ Losfuturistas/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'toafqynpxd*0k8ktz@82#farw%-#l_fy72&r25)#oh70uvd3az'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
